7e24e0055cf9d21a3068c13256358c3de74be10d
luxman <luxman.elangeswaralingam@kcl.ac.uk>
2016-03-12 16:52:10 +0000

    added the file to read CSV files

--- app/src/main/java/com/projectrun/moneytalks/MainActivity.java
+++ app/src/main/java/com/projectrun/moneytalks/MainActivity.java
@@ -1,7 +1,20 @@
 package com.projectrun.moneytalks;
 
+import android.content.Context;
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.opencsv.CSVReader;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +22,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        readCsv(this);
+    }
+    public final List<String[]> readCsv(Context context) {
+        List<String[]> questionList = new ArrayList<String[]>();
+        AssetManager assetManager = context.getAssets();
+
+        try {
+            InputStream csvStream = assetManager.open("NationwideData.csv");
+            InputStreamReader csvStreamReader = new InputStreamReader(csvStream);
+            CSVReader csvReader = new CSVReader(csvStreamReader);
+            String[] line;
+
+            // throw away the header
+            csvReader.readNext();
+
+            while ((line = csvReader.readNext()) != null) {
+                questionList.add(line);
+                Log.d("works" , questionList.get(0)[0]);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return questionList;
     }
 }
